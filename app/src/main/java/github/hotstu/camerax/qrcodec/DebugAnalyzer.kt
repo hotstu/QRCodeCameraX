e3f713a15913400c2dbe887f30775440d7d94bd7
hglf <hgleifeng@qq.com>
2020-04-09 13:58:21 +0800

    add debug

---
+++ app/src/main/java/github/hotstu/camerax/qrcodec/DebugAnalyzer.kt
@@ -0,0 +1,65 @@
+package github.hotstu.camerax.qrcodec
+
+import android.graphics.Bitmap
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import java.nio.ByteBuffer
+
+
+/**
+ * @author hglf [hglf](https://github.com/hotstu)
+ * @desc
+ * @since 6/10/19
+ */
+class DebugAnalyzer(val listener: (Bitmap)-> Unit) : ImageAnalysis.Analyzer {
+    /**
+     * Analyzes an image to produce a result.
+     *
+     * <p>The caller is responsible for ensuring this analysis method can be executed quickly
+     * enough to prevent stalls in the image acquisition pipeline. Otherwise, newly available
+     * images will not be acquired and analyzed.
+     *
+     * <p>The image passed to this method becomes invalid after this method returns. The caller
+     * should not store external references to this image, as these references will become
+     * invalid.
+     *
+     * @param image image being analyzed VERY IMPORTANT: Analyzer method implementation must
+     * call image.close() on received images when finished using them. Otherwise, new images
+     * may not be received or the camera may stall, depending on back pressure setting.
+     *
+     */
+    override fun analyze(image: ImageProxy, rotationDegrees: Int) {
+        listener(image.toBitmap())
+    }
+
+    /**
+     * Helper extension function used to extract a byte array from an image plane buffer
+     */
+    private fun ByteBuffer.toByteArray(): ByteArray {
+        rewind()    // Rewind the buffer to zero
+        val data = ByteArray(remaining())
+        get(data)   // Copy the buffer into a byte array
+        return data // Return the byte array
+    }
+
+    private fun ImageProxy.toBitmap(): Bitmap {
+        val yBuffer = planes[0].buffer // Y
+        val uBuffer = planes[1].buffer // U
+        val vBuffer = planes[2].buffer // V
+
+        val ySize = yBuffer.remaining()
+        val uSize = uBuffer.remaining()
+        val vSize = vBuffer.remaining()
+
+        val nv21 = ByteArray(ySize + uSize + vSize)
+
+        yBuffer.get(nv21, 0, ySize)
+        vBuffer.get(nv21, ySize, vSize)
+        uBuffer.get(nv21, ySize + vSize, uSize)
+
+        val rgba = IntArray(width * height)
+        decodeYUV420(rgba, nv21, width, height)
+        return Bitmap.createBitmap(rgba, width, height, Bitmap.Config.ARGB_8888)
+
+    }
+}
