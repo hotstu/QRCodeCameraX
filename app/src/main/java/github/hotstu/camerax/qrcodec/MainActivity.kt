8d38608c9f75ecdc23420adb8c8f39d83148fc44
Ivan <79067180651@ya.ru>
2021-01-08 11:16:04 +0300

    Migrate MLQRcodeAnalyzer from firebase to mlkit, migrate cameraview to rc-01, update gradle

--- app/src/main/java/github/hotstu/camerax/qrcodec/MainActivity.kt
+++ app/src/main/java/github/hotstu/camerax/qrcodec/MainActivity.kt
@@ -1,12 +1,11 @@
 package github.hotstu.camerax.qrcodec
 
 import android.annotation.SuppressLint
-import android.hardware.display.DisplayManager
+import android.os.Build
 import android.os.Bundle
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.View
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.AspectRatio
 import androidx.camera.core.CameraSelector
@@ -16,8 +15,6 @@ import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
-import com.google.android.gms.common.ConnectionResult
-import com.google.android.gms.common.GoogleApiAvailability
 import com.tbruyelle.rxpermissions2.RxPermissions
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -25,21 +22,15 @@ import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-const val FLAGS_FULLSCREEN =
-        View.SYSTEM_UI_FLAG_LOW_PROFILE or
-                View.SYSTEM_UI_FLAG_FULLSCREEN or
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-
-const val IMMERSIVE_FLAG_TIMEOUT = 500L
-
 class MainActivity : AppCompatActivity() {
 
-    var analysis: ImageAnalysis? = null
+    private var analysis: ImageAnalysis? = null
     /** Blocking camera operations are performed using this executor */
     private lateinit var cameraExecutor: ExecutorService
+
+    private val qrResult get() = findViewById<TextView>(R.id.qr_result)
+
+    private val useMlKitDetector = true
 
     @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +39,6 @@ class MainActivity : AppCompatActivity() {
         // Initialize our background executor
         cameraExecutor = Executors.newSingleThreadExecutor()
         val viewFinder = findViewById<PreviewView>(R.id.view_finder)
-        val mono = findViewById<ImageView>(R.id.mono)
 
         val rxPermissions = RxPermissions(this)
         rxPermissions.request(android.Manifest.permission.CAMERA)
@@ -61,7 +51,7 @@ class MainActivity : AppCompatActivity() {
                         val screenAspectRatio = aspectRatio(metrics.widthPixels, metrics.heightPixels)
                         val rotation = viewFinder.display.rotation
 
-                        cameraProviderFuture.addListener(Runnable {
+                        cameraProviderFuture.addListener({
                             // CameraProvider
                             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
                             val preview = Preview.Builder().apply {
@@ -70,7 +60,7 @@ class MainActivity : AppCompatActivity() {
                             }.build()
 
                             // Attach the viewfinder's surface provider to preview use case
-                            preview.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                            preview.setSurfaceProvider(viewFinder.surfaceProvider)
 
                             val analysis = ImageAnalysis.Builder().apply {
                                 setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
@@ -78,23 +68,10 @@ class MainActivity : AppCompatActivity() {
                                 setTargetRotation(rotation)
                             }.build()
 
-                            val debugAnalysis = ImageAnalysis.Builder().apply {
-                                setBackpressureStrategy(STRATEGY_KEEP_ONLY_LATEST)
-                                setTargetAspectRatio(screenAspectRatio)
-                                setTargetRotation(rotation)
-                            }.build()
-                            debugAnalysis.setAnalyzer(cameraExecutor, DebugAnalyzer{
-                                mono.post {
-                                    mono.setImageBitmap(it)
-                                }
-                            })
-                            val googlePlayServicesAvailable = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
-                            if (googlePlayServicesAvailable == ConnectionResult.SUCCESS) {
-                                Log.d("MainActivity", "google play services avalable, using visionBarcodeDetector")
-                                analysis.setAnalyzer(cameraExecutor, MLQRcodeAnalyzer())
-                            } else {
-                                Log.d("MainActivity", "google play services inavalable, fallback to zxing")
-                                analysis.setAnalyzer(cameraExecutor, QRcodeAnalyzer())
+                            if(useMlKitDetector){
+                                analysis.setAnalyzer(cameraExecutor, MLQRcodeAnalyzer(this::onResult))
+                            } else{
+                                analysis.setAnalyzer(cameraExecutor, QRcodeAnalyzer(this::onResult))
                             }
                             // Must unbind the use-cases before rebinding them
                             cameraProvider.unbindAll()
@@ -112,24 +89,27 @@ class MainActivity : AppCompatActivity() {
         // Before setting full screen flags, we must wait a bit to let UI settle; otherwise, we may
         // be trying to set app to immersive mode before it's ready and the flags do not stick
         window.decorView.postDelayed({
-            window.decorView.systemUiVisibility = FLAGS_FULLSCREEN
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                window.setDecorFitsSystemWindows(false)
+            } else {
+                @Suppress("DEPRECATION")
+                window.decorView.systemUiVisibility = FLAGS_FULLSCREEN
+            }
         }, IMMERSIVE_FLAG_TIMEOUT)
     }
-    /**
-     * We need a display listener for orientation changes that do not trigger a configuration
-     * change, for example if we choose to override config change in manifest or for 180-degree
-     * orientation changes.
-     */
-    private val displayListener = object : DisplayManager.DisplayListener {
-        override fun onDisplayAdded(displayId: Int) = Unit
-        override fun onDisplayRemoved(displayId: Int) = Unit
-        override fun onDisplayChanged(displayId: Int) {
-            //analysis?.targetRotation = .display.rotation
+
+
+    private fun onResult(value: String?) {
+        qrResult.post {
+            if(qrResult.text != value){
+                qrResult.text = value
+            }
         }
     }
+
     /**
-     *  [androidx.camera.core.ImageAnalysisConfig] requires enum value of
-     *  [androidx.camera.core.AspectRatio]. Currently it has values of 4:3 & 16:9.
+     *  [androidx.camera.core.ImageAnalysis.Builder] requires enum value of
+     *  [androidx.camera.core.AspectRatio.Ratio]. Currently it has values of 4:3 & 16:9.
      *
      *  Detecting the most suitable ratio for dimensions provided in @params by counting absolute
      *  of preview ratio to one of the provided values.
@@ -156,5 +136,16 @@ class MainActivity : AppCompatActivity() {
     companion object {
         private const val RATIO_4_3_VALUE = 4.0 / 3.0
         private const val RATIO_16_9_VALUE = 16.0 / 9.0
+
+        private const val IMMERSIVE_FLAG_TIMEOUT = 500L
+
+        @Suppress("DEPRECATION")
+        private const val FLAGS_FULLSCREEN =
+            View.SYSTEM_UI_FLAG_LOW_PROFILE or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
     }
 }
